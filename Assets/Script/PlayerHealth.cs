3d7e5d96d0ce1e76ea12c535611ff49bc36f9f65
Wesily-G <729009583@qq.com>
2024-02-21 13:27:44 -0500

    Injury

--- Assets/Script/PlayerHealth.cs
+++ Assets/Script/PlayerHealth.cs
@@ -2,17 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerHealth : Health
 {
     public AudioClip takeHitClip;
+    public GameObject bloodyScreen;
 
     public override void TakeDamage(float damage)
     {
         base.TakeDamage(damage);
         if (takeHitClip != null && GetComponent<AudioSource>())
         {
+            Debug.Log("TakeDamageInPlayerHealth");
             GetComponent<AudioSource>().PlayOneShot(takeHitClip);
+            BloodyScreenEffect();
         }
     }
     public override void Death()
@@ -30,5 +34,47 @@ public class PlayerHealth : Health
 
         GetComponent<AudioSource>().PlayOneShot(deathClip);
     }
+
+    private IEnumerator BloodyScreenEffect(){
+
+    // 检查bloodyScreen是否处于非激活状态，如果是则将其激活
+    if (!bloodyScreen.activeInHierarchy)
+    {
+        bloodyScreen.SetActive(true);
+    }
+
+    // 获取bloodyScreen下的Image组件
+    var image = bloodyScreen.GetComponentInChildren<Image>();
+
+    // 设置初始alpha值为1（完全可见）
+    Color startColor = image.color;
+    startColor.a = 1f;
+    image.color = startColor;
+
+    float duration = 3f;
+    float elapsedTime = 0f;
+
+    while (elapsedTime < duration)
+    {
+        // 使用Lerp计算新的alpha值
+        float alpha = Mathf.Lerp(1f, 0f, elapsedTime / duration);
+
+        // 更新颜色的alpha值
+        Color newColor = image.color;
+        newColor.a = alpha;
+        image.color = newColor;
+
+        // 增加经过的时间
+        elapsedTime += Time.deltaTime;
+
+        yield return null; // 等待下一帧
+    }
+
+    // 检查bloodyScreen是否处于激活状态，如果是则将其关闭
+    if (bloodyScreen.activeInHierarchy)
+    {
+        bloodyScreen.SetActive(false);
+    }
+}
 }
 
